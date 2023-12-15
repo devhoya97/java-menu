56f56618ddc08f97dd1859f391854ba4b384104f
devhoya97 <diajbh123@gmail.com>
2023-12-15 17:17:34 +0900

    feat: 코치 리스트를 입력받는 기능 구현

--- src/main/java/menu/view/InputView.java
+++ src/main/java/menu/view/InputView.java
@@ -1,4 +1,37 @@
 package menu.view;
 
+import camp.nextstep.edu.missionutils.Console;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import menu.Coach;
+
 public class InputView {
+    private static final String DELIMITER = ",";
+    public static List<Coach> readCoaches() {
+        System.out.println("\n코치의 이름을 입력해 주세요. (, 로 구분)");
+        while (true) {
+            try {
+                List<String> coachNames = getTrimmedSplitInput();
+                return coachNames.stream()
+                        .map(Coach::new)
+                        .collect(Collectors.toList());
+            } catch (IllegalArgumentException illegalArgumentException) {
+                OutputView.printErrorMessage(illegalArgumentException.getMessage());
+            }
+        }
+    }
+
+    private static List<String> getTrimmedSplitInput() {
+        List<String> splitInput = Arrays.asList(Console.readLine().split(DELIMITER));
+        return splitInput.stream()
+                .map(String::trim)
+                .collect(Collectors.toList());
+    }
+
+    private static void validateSize(List<String> coachNames) {
+        if (coachNames.size() < 2 || coachNames.size() > 5) {
+            throw new IllegalArgumentException("코치는 최소 2명, 최대 5명까지 입력받을 수 있습니다.");
+        }
+    }
 }
