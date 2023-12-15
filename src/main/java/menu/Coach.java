5df17c0a521dfaf44aab6eb95b3f5d114c61769f
devhoya97 <diajbh123@gmail.com>
2023-12-15 16:39:02 +0900

    feat: 코치 객체의 요일 별 먹은 음식 리스트에 음식을 추가하는 기능 구현

--- src/main/java/menu/Coach.java
+++ src/main/java/menu/Coach.java
@@ -1,4 +1,15 @@
 package menu;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Coach {
+    private final List<String> menus = new ArrayList<>();
+
+    public boolean addMenu(String menu) {
+        if (menus.contains(menu)) {
+            return false;
+        }
+        return menus.add(menu);
+    }
 }
