8189a94af56d5e2fbfc6358e219ac16b8a86842b
devhoya97 <diajbh123@gmail.com>
2023-12-15 17:03:39 +0900

    feat: 프로그램 시작, 종료 메시지 출력 기능 구현

---
+++ src/main/java/menu/view/OutputView.java
@@ -0,0 +1,11 @@
+package menu.view;
+
+public class OutputView {
+    public static void printStart() {
+        System.out.println("점심 메뉴 추천을 시작합니다.");
+    }
+
+    public static void printEnd() {
+        System.out.println("추천을 완료했습니다.");
+    }
+}
